5e488abe7e9e3ec190a9dd4609b51b8f466ace6c
Thomas Toye <toye.thomas@gmail.com>
2014-11-19 22:47:12 +0100

    Test Customer.Equals

---
+++ DatabaseTest/CustomerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nmct.ba.cashlessproject.model;
+
+namespace CashlessTests
+{
+    [TestClass]
+    public class Customertest
+    {
+        [TestMethod]
+        public void Equality()
+        {
+            Customer c1 = new Customer { Address = "Straatlaan 55", Balance = 50, Name = "Thomas", ID = 5 };
+            Customer c2 = new Customer { Address = "Straatlaan 55", Balance = 50, Name = "Thomas", ID = 6 };
+
+            Assert.IsFalse(c1.Equals(c2));
+            Assert.IsFalse(c2.Equals(c1));
+
+            c2.ID = 5;
+
+            Assert.IsTrue(c1.Equals(c2));
+            Assert.IsTrue(c2.Equals(c1));
+        }
+    }
+}
