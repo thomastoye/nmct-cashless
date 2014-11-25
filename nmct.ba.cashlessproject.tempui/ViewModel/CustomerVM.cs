eb09d7e316ac26ae0e7e765da4e07b39bac6c208
Thomas Toye <toye.thomas@gmail.com>
2014-11-25 21:29:57 +0100

    Add customer ViewModel

---
+++ nmct.ba.cashlessproject.tempui/ViewModel/CustomerVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.tempui.ViewModel
+{
+    class CustomerVM : ObservableObject
+    {
+        public CustomerVM() {
+            GetCustomers();
+        }
+
+        private async void GetCustomers()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await
+                client.GetAsync("http://localhost:15269/api/customer");
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    Customers = JsonConvert.DeserializeObject<ObservableCollection<Customer>>(json);
+                }
+            }
+        }
+
+        private ObservableCollection<Customer> _customers;
+        public ObservableCollection<Customer> Customers
+        {
+            get { return _customers; }
+            set { _customers = value; OnPropertyChanged("Customers"); }
+        }
+        private Customer _selectedCustomer;
+        public Customer SelectedCustomer
+        {
+            get { return _selectedCustomer; }
+            set { _selectedCustomer = value; OnPropertyChanged("SelectedCustomer"); }
+        }
+
+    }
+}
