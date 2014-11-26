a58f03792c8f839acceb08b6fbbfe744fa0bdedb
Thomas Toye <toye.thomas@gmail.com>
2014-11-26 19:45:10 +0100

    Break out appropriate files to seperate WPF project

---
+++ nmct.ba.cashlessproject.vereniging/ViewModel/KassasVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.vereniging.ViewModel
+{
+    class KassasVM : ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "Kassas"; }
+        }
+    }
+}
---
+++ nmct.ba.cashlessproject.vereniging/ViewModel/PageOneVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.vereniging.ViewModel
+{
+    class PageOneVM : ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "First page"; }
+        }
+        private string _demo = "Working";
+        public string Demo
+        {
+            get { return _demo; }
+            set { _demo = value; OnPropertyChanged("Demo"); }
+        }
+    }
+}
