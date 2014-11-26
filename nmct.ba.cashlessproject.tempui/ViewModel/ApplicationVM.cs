dc84f6b960bd98fb1c760ca8099291ffc9568913
Thomas Toye <toye.thomas@gmail.com>
2014-11-26 15:00:57 +0100

    Move UI stuff to permanent UI project

---
+++ nmct.ba.cashlessproject.tempui/ViewModel/ApplicationVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace nmct.ba.cashlessproject.tempui.ViewModel
+{
+    public class ApplicationVM : ObservableObject
+    {
+        public ApplicationVM()
+        {
+        Pages.Add(new PartOneVM());
+        Pages.Add(new PartTwoVM());
+        Pages.Add(new PartThreeVM());
+        CurrentPage = Pages[0];
+        }
+        private IPage currentPage;
+        public IPage CurrentPage
+        {
+            get { return currentPage; }
+            set { currentPage = value; OnPropertyChanged("CurrentPage"); }
+        }
+        private List<IPage> pages;
+        public List<IPage> Pages
+        {
+            get
+            {
+                if (pages == null)
+                    pages = new List<IPage>();
+                return pages;
+            }
+        }
+        public ICommand ChangePageCommand
+        {
+            get { return new RelayCommand<IPage>(ChangePage); }
+        }
+        private void ChangePage(IPage page)
+        {
+            CurrentPage = page;
+        }
+    }
+
+}
---
+++ nmct.ba.cashlessproject.ui/View/Kassa.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace nmct.ba.cashlessproject.tempui
+{
+    /// <summary>
+    /// Interaction logic for Kassa.xaml
+    /// </summary>
+    public partial class Kassa : Window
+    {
+        public Kassa()
+        {
+            InitializeComponent();
+        }
+    }
+}
