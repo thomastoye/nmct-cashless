c3c2fffa56e9bee8bdd1bb89fbd78bc03959968d
Thomas Toye <toye.thomas@gmail.com>
2014-11-26 20:18:43 +0100

    Add Viewmodels

---
+++ nmct.ba.cashlessproject.vereniging/ViewModel/KlantenVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.vereniging.ViewModel
+{
+    class KlantenVM : ObservableObject, IPage
+    {
+
+        public string Name
+        {
+            get { return "Klanten"; }
+        }
+    }
+}
