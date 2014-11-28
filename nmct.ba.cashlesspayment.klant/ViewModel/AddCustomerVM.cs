7e6fbfaa3eecc1e6580d9faeddd31dd75dc43cdd
Thomas Toye <toye.thomas@gmail.com>
2014-11-28 12:53:08 +0100

    Add customer UI project

---
+++ nmct.ba.cashlesspayment.klant/ViewModel/AddCustomerVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlesspayment.klant.ViewModel
+{
+    class AddCustomerVM : ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "Nieuwe klant"; }
+        }
+    }
+}
---
+++ nmct.ba.cashlesspayment.klant/ViewModel/ApplicationVM.cs
@@ -0,0 +1,48 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace nmct.ba.cashlesspayment.klant.ViewModel
+{
+    class ApplicationVM : ObservableObject
+    {
+        public ApplicationVM()
+        {
+            Pages.Add(new AddCustomerVM());
+
+            CurrentPage = Pages[0];
+        }
+
+        private object currentPage;
+        public object CurrentPage
+        {
+            get { return currentPage; }
+            set { currentPage = value; OnPropertyChanged("CurrentPage"); }
+        }
+
+        private List<IPage> pages;
+        public List<IPage> Pages
+        {
+            get
+            {
+                if (pages == null)
+                    pages = new List<IPage>();
+                return pages;
+            }
+        }
+
+        public ICommand ChangePageCommand
+        {
+            get { return new RelayCommand<IPage>(ChangePage); }
+        }
+
+        private void ChangePage(IPage page)
+        {
+            CurrentPage = page;
+        }
+    }
+}
