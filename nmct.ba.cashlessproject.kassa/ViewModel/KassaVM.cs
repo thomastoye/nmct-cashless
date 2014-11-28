6fc7e8dd38bdea080ac1c8b143f3673f019f67ff
Thomas Toye <toye.thomas@gmail.com>
2014-11-28 23:54:38 +0100

    Add kassa project

---
+++ nmct.ba.cashlessproject.kassa/ViewModel/KassaVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nmct.ba.cashlessproject.kassa.ViewModel
+{
+    class KassaVM : ObservableObject, IPage
+    {
+        public string Name
+        {
+            get { return "Kassa"; }
+        }
+    }
+}
