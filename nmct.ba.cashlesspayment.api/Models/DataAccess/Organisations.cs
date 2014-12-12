f541474d8b6c149e024beca5de54262ab6943c50
Thomas Toye <toye.thomas@gmail.com>
2014-12-12 11:14:06 +0100

    Move organisation into model library

--- nmct.ba.cashlesspayment.api/Models/DataAccess/Organisations.cs
+++ nmct.ba.cashlesspayment.api/Models/DataAccess/Organisations.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Web;
+using nmct.ba.cashlessproject.model;
 
 namespace nmct.ba.cashlessproject.api.Models.DataAccess
 {
