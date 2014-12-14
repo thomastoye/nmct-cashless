584460b5729356a091614c07fe9293bd8df1e041
Thomas Toye <toye.thomas@gmail.com>
2014-12-14 18:04:08 +0100

    Clean up back-end

--- nmct.ba.cashlessproject.api/App_Start/RouteConfig.cs
+++ nmct.ba.cashlessproject.api/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace nmct.ba.cashlessproject.api
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Organisation", action = "List", id = UrlParameter.Optional }
             );
         }
     }
