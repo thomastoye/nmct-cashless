0ff6b95f2b5b7ce5c37e952b0703955603304b30
Thomas Toye <toye.thomas@gmail.com>
2014-12-28 18:15:44 +0100

    Allow posting logs

---
+++ nmct.ba.cashlessproject.api/Controllers/LogController.cs
@@ -0,0 +1,22 @@
+﻿using nmct.ba.cashlessproject.api.Models.DataAccess;
+using nmct.ba.cashlessproject.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace nmct.ba.cashlessproject.api.Controllers
+{
+    public class LogController : ApiController
+    {
+        public void Post(ErrorLog log)
+        {
+            if (ModelState.IsValid)
+            {
+                LogDA.Insert(log);
+            }
+        }
+    }
+}
