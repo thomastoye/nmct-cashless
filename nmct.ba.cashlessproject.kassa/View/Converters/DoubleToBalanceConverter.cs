e654a829db7c535445f3998ba8141014410ab7aa
Thomas Toye <toye.thomas@gmail.com>
2015-01-02 20:45:51 +0100

    Format balance as currency

---
+++ nmct.ba.cashlessproject.kassa/View/Converters/DoubleToBalanceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace nmct.ba.cashlessproject.kassa.Converters
+{
+    class DoubleToBalanceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "€" +  String.Format("{0:0.00}", value.ToString());
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
